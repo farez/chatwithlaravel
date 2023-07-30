2c4c3d2f205105a1e9f9d518574e89f44cada2b5
Farez Rahman <farez@redkeydigital.com>
2023-07-30 19:35:50 +0800

    Renamed summary column to transcript.

--- database/migrations/2023_07_29_151439_add_transcript_column_to_videos_table.php
+++ database/migrations/2023_07_29_151439_add_transcript_column_to_videos_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('videos', function (Blueprint $table) {
-            $table->text('summary')->nullable();
+            $table->text('transcript')->nullable();
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('videos', function (Blueprint $table) {
-            $table->dropColumn('summary');
+            $table->dropColumn('transcript');
         });
     }
 };
